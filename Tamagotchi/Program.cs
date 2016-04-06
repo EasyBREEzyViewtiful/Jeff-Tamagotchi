58fe476377372ccbb377c3f4a61cc6cadef0c378
= <jeffchen330@gmail.com>
2016-04-05 22:14:26 -0400

    first commit

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tamagotchi t = new Tamagotchi("Bob", "Male", 100, 100);
+
+            while (t.GetFullness() > 0 && t.GetHappiness() > 0)
+            {
+                Console.WriteLine("Would you like to feed or pet {0}?", t.GetName());
+                String action = Console.ReadLine();
+
+                if (action == "feed")
+                {
+                    t.Feed();
+                }
+                else if (action == "pet")
+                {
+                    t.Pet();
+                }
+                else
+                {
+                    Console.WriteLine("You can't do that with " + t.GetName() + ". Please try again!");
+                }
+                t.DoStuff();
+
+                Console.WriteLine("Bob's new stats are:");
+                Console.WriteLine("Fullness: {0} | Happiness: {1}", t.GetFullness(), t.GetHappiness());
+            }
+
+        }
+    }
+}
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    class Tamagotchi
+    {
+        private string Name;
+        private string Gender;
+        private int Fullness;
+        private int Happiness;
+
+        public Tamagotchi(string name, string gender, int fullness, int happiness)
+        {
+            Name = name;
+            Gender = gender;
+            Fullness = fullness;
+            Happiness = happiness;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetGender()
+        {
+            return Gender;
+        }
+
+        public int GetFullness()
+        {
+            return Fullness;
+        }
+
+        public int GetHappiness()
+        {
+            return Happiness;
+        }
+
+        public void Pet()
+        {
+            Random rnd = new Random();
+            Happiness += rnd.Next(5);
+        }
+
+        public void Feed()
+        {
+            Random rnd = new Random();
+            Fullness += rnd.Next(5);
+        }
+
+        public void DoStuff()
+        {
+            Random rnd = new Random();
+            Fullness -= rnd.Next(5);
+            Happiness -= rnd.Next(5);
+        }
+    }
+}
